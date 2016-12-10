008dacab971d60b8b729689a77cc0dfadca1ab7f
Dan Ertman <dtertman@yahoo.com>
2016-12-10 01:07:56 -0600

    settings update

--- drchrono/settings.py
+++ drchrono/settings.py
@@ -109,3 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SOCIAL_AUTH_DRCHRONO_KEY = os.environ.get('DR_CHRONO_KEY')
+SOCIAL_AUTH_DRCHRONO_SECRET = os.environ.get('DR_CHRONO_SECRET')
+SOCIAL_AUTH_DRCHRONO_SCOPE=['patients:summary:read']
